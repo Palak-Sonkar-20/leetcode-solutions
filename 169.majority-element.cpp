256ac2723c4c1347b06bd1e175349b9b12ef6baf
Palak-Sonkar-20 <palaksonkar20@gmail.com>
2026-07-16 12:55:11 +0530

    Array/ sort

---
+++ 169.majority-element.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=169 lang=cpp
+ *
+ * [169] Majority Element
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int candidate = 0;
+        for(int num: nums){
+            if(count ==0){
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+    }
+};
+// @lc code=end
+
--- 56.merge-intervals.cpp
+++ 56.merge-intervals.cpp
@@ -8,7 +8,18 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
+        if( intervals.empty()) return {};
+        sort(intervals.begin() , intervals.end());
+        vector<vector<int>> res;
+        res.push_back(intervals[0]);
+        for( auto interval : intervals){
+            if( res.back()[1] >= interval[0]){
+                res.back()[1] = max(res.back()[1], interval[1]);
+            } else {
+                res.push_back(interval);
+            }
+        }
+        return res;
     }
 };
 // @lc code=end
